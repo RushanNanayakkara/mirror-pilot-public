16dbf12d13370c19c1d1d4f5c0d02e670a63e2bc
rushannanayakkara <rushannana@gmail.com>
2026-08-03 23:23:30 +0530

    Add a warn level to Logger

--- src/Logger.java
+++ src/Logger.java
@@ -2,4 +2,7 @@ public class Logger {
     public void info(String msg) {
         System.out.println("[info] " + msg);
     }
+    public void warn(String msg) {
+        System.out.println("[warn] " + msg);
+    }
 }
