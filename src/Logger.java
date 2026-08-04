8e0fab397a6409063c11fc1fb552ce048dc6066b
rushannanayakkara <rushannana@gmail.com>
2026-08-04 22:20:49 +0530

    Spell out the info log prefix

--- src/Logger.java
+++ src/Logger.java
@@ -1,6 +1,6 @@
 public class Logger {
     public void info(String msg) {
-        System.out.println("[info] " + msg);
+        System.out.println("[information] " + msg);
     }
     public void warn(String msg) {
         System.out.println("[warn] " + msg);
